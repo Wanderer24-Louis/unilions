686a516aab422fb843bf3b501c08aaa530d47d0f
User <user@example.com>
2025-10-11 23:36:16 +0800

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // RSS新聞來源
 const RSS_SOURCES = [
