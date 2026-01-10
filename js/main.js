0d33b772e4f05b83318713f6e5e424d19dd2eea6
User <user@example.com>
2026-01-10 16:38:36 +0800

    update

--- js/main.js
+++ js/main.js
@@ -284,3 +284,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// 回到頁首按鈕
+const backToTopButton = document.getElementById("back-to-top");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.onclick = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
