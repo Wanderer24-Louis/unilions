11585a9d8624f87584b89a796bb7bc5669df1daa
User <user@example.com>
2026-01-11 14:50:21 +0800

    update

--- js/main.js
+++ js/main.js
@@ -297,6 +297,8 @@ window.onscroll = function() {
 };
 
 backToTopButton.onclick = function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 };
